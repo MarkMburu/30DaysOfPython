893cc309d6c18dae8df91792069fdcccbc98c995
MarkMburu <mackryanlamar1@gmail.com>
2020-03-25 11:47:47 +0300

    first commit

--- Day 7/main.py
+++ Day 7/main.py
@@ -5,9 +5,9 @@ def my_print(txt):
 msg_template = """Hello {name},
 Thank you for joining {website}. We are very
 happy to have you with us.
-""" # .format(name="Justin", website='cfe.sh')
+""" # .format(name="mark", website='markmburu.github.io')
 
-def format_msg(my_name="Justin", my_website="cfe.sh"):
+def format_msg(my_name="Mark", my_website="markmburu.github.io"):
     my_msg = msg_template.format(name=my_name, website=my_website)
     # print(my_msg)
     return my_msg
---
+++ README.md
@@ -0,0 +1,3 @@
+[![30 Days of Python Logo](https://static.codingforentrepreneurs.com/media/projects/30-days-python-38/images/share/30_Days_of_Python_-_Share.jpg)]
+
+# 30 Days of Python
